02cb2d600ef68c9612aeb560fb93d3a8a3e400c6
Brandon Sneed <brandon@redf.net>
2017-02-01 13:53:17 -0800

    Update modulo.rb

--- Formula/modulo.rb
+++ Formula/modulo.rb
@@ -4,12 +4,6 @@ class Modulo < Formula
   url "https://github.com/modulo-dm/modulo/archive/v0.5.0.tar.gz"
   sha256 "d9d6362b962a5e747c785b56dc8084453870d260a83a3af8886dce678623c331"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b885b0927389818fc6cf186217eec981b8f2d18ae682333b7d708f6a0356b84f" => :sierra
-    sha256 "c2049122beebd7eadaa49dbd7e14612d1e6b2a07919a2f82cde1021028cb0c61" => :el_capitan
-  end
-
   depends_on :xcode => "8.0"
 
   def install
